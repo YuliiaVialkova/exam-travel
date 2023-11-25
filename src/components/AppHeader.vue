c02bc5d4abd7449d98a07317f7b0fb5667939330
Julia Vialkova <julia.zagrafova@gmail.com>
2023-11-25 16:26:57 +0200

    created header menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HeaderLogo from './HeaderLogo.vue'
+import HeaderMenu from './HeaderMenu.vue'
 </script>
 
 <template>
   <div class="header">
     <HeaderLogo class="header__logo" />
+    <HeaderMenu class="header__menu" />
   </div>
 </template>
---
+++ src/components/HeaderMenu.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="header-menu">
+    <ul class="header-menu__list">
+      <li class="header-menu__item">
+        <a href="" class="header-menu__link">Discover</a>
+      </li>
+      <li class="header-menu__item">
+        <a href="" class="header-menu__link">Activities</a>
+      </li>
+      <li class="header-menu__item">
+        <a href="" class="header-menu__link">About</a>
+      </li>
+      <li class="header-menu__item">
+        <a href="" class="header-menu__link">Contacts</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss">
+.header-menu {
+  display: none;
+
+  &__list {
+    list-style: none;
+  }
+  &__link {
+    text-decoration: none;
+    color: inherit;
+  }
+}
+</style>
