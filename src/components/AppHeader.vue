2c827d61b993925a3ec10ea76f2122645a6d83c2
Julia Vialkova <julia.zagrafova@gmail.com>
2023-11-25 16:36:26 +0200

    created header auth

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,13 @@
 <script setup>
 import HeaderLogo from './HeaderLogo.vue'
 import HeaderMenu from './HeaderMenu.vue'
+import HeaderAuth from './HeaderAuth.vue'
 </script>
 
 <template>
   <div class="header">
     <HeaderLogo class="header__logo" />
     <HeaderMenu class="header__menu" />
+    <HeaderAuth class="header__auth" />
   </div>
 </template>
---
+++ src/components/HeaderAuth.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="header-auth">
+    <div class="header-auth__register">
+      <a href="" class="header-auth__register-link">Реєстрація</a>
+    </div>
+    <div class="header-auth__sign">
+      <a href="" class="header-auth__sign-link">Вхід</a>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.header-auth {
+  display: flex;
+  gap: 20px;
+
+  &__register {
+    padding: 10px 18px;
+    border-radius: 6px;
+    border: 1px solid var(--btn-bg-color);
+  }
+  &__register-link {
+    text-decoration: none;
+    color: var(--blue-color);
+    font-size: toRem(15px);
+    font-weight: 500;
+    line-height: 20px;
+    letter-spacing: 0.3px;
+  }
+  &__sign {
+    padding: 10px 18px;
+    border-radius: 6px;
+    background-color: var(--btn-bg-color);
+  }
+  &__sign-link {
+    text-decoration: none;
+    font-size: toRem(15px);
+    color: var(--white-color);
+    font-weight: 500;
+    line-height: 20px;
+    letter-spacing: 0.3px;
+  }
+}
+</style>
